585b31f4762ff6d75c8db6fd2e71632ec92e3d37
Akhona Mnguni <akhonamnguni00@gmail.com>
2026-04-10 11:11:26 +0200

    Arithmetic

---
+++ src/main/java/com/za/Arithmetic.java
@@ -0,0 +1,43 @@
+package com.za;
+import java.util.Scanner;
+
+public class Arithmetic {
+    public static void main(String[] args){
+        // Arithmetic Operators
+
+        int x = 10;
+        int y = 2;
+        int z;
+
+         z = x + y;
+//         z = x - y;
+//         z = x * y;
+         z = x / y;
+//         z = x % y;
+        System.out.println(z);
+
+        // Augemented Assignment
+
+//        x += y;
+//        x -= y;
+//        x *= y;
+//        x /= y;
+//        x %= y;
+//        System.out.println(x);
+
+//        // Increment and Decrement Operators
+//        int x = 1;
+//       x = x + 1;
+//            x++;
+//            x++;
+//            x--;
+//        System.out.println(x);
+
+        // Order of Operations [P-E-M-D-A-s]
+
+        double result = 3 + 4 * (7 - 5) / 2.0;
+        System.out.println(result);
+
+//        scanner.close();
+    }
+}
